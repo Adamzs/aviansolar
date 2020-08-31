f5b89768bb0e13a774b7d1259b5b0dbc65238bea
Adam Szymanski <szymanski@gmail.com>
2020-08-31 12:26:35 -0500

    Added wait one minute line to script to make sure network is ready for mounting drive

--- camera_files/gstreamer-record-splits.sh
+++ camera_files/gstreamer-record-splits.sh
@@ -10,4 +10,5 @@
 ### END INIT INFO
 
 today=`date +%Y_%m_%d_%H_%M_%S`
+sleep 1m
 mount 10.0.0.86:/media/usb /media && gst-launch-1.0 nvarguscamerasrc ! 'video/x-raw(memory:NVMM), width=(int)3840, height=(int)2160, framerate=30/1, format=(string)NV12' ! nvtee ! nvvidconv ! 'video/x-raw(memory:NVMM), format=(string)I420_10LE' ! omxh265enc preset-level=3 bitrate=8000000 ! splitmuxsink location=/media/video-%05d-$today.mkv max-size-time=300000000000 > /home/boulderai/log.txt 2> /home/boulderai/error-log.txt
