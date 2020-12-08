6830664280167c61b85dcfef8aae606951279fcb
Adam Szymanski <adamzs@gmail.com>
2020-12-08 20:20:51 +0000

    Increasing bitrate on new camera recording script

--- new-camera-setup/test_service.sh
+++ new-camera-setup/test_service.sh
@@ -15,7 +15,7 @@ echo $(($VAR + 1)) > /data/bcount.txt
 
 bootnum=$VAR
 today=`date +%Y_%m_%d_%H_%M_%S`
-docker run --net=host --runtime nvidia --ipc=host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /tmp/argus_socket:/tmp/argus_socket -v /dev/video0:/dev/video0 -v /data:/data -v mynfs:/data/mount/nfs --privileged=true -e DISPLAY=$DISPLAY -t -d drive-mount && gst-launch-1.0 nvarguscamerasrc ! 'video/x-raw(memory:NVMM), width=(int)3840, height=(int)2160, framerate=30/1, format=(string)NV12' ! nvtee ! nvvidconv ! 'video/x-raw(memory:NVMM), format=(string)I420_10LE' ! omxh265enc preset-level=3 bitrate=8000000 ! splitmuxsink location=/data/var/lib/docker/volumes/mynfs/_data/video-$bootnum-%05d-$today.mkv max-size-time=300000000000 > /data/log.txt 2> /data/error-log.txt
+docker run --net=host --runtime nvidia --ipc=host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /tmp/argus_socket:/tmp/argus_socket -v /dev/video0:/dev/video0 -v /data:/data -v mynfs:/data/mount/nfs --privileged=true -e DISPLAY=$DISPLAY -t -d drive-mount && gst-launch-1.0 nvarguscamerasrc ! 'video/x-raw(memory:NVMM), width=(int)3840, height=(int)2160, framerate=30/1, format=(string)NV12' ! nvtee ! nvvidconv ! 'video/x-raw(memory:NVMM), format=(string)I420_10LE' ! omxh265enc preset-level=3 bitrate=28000000 ! splitmuxsink location=/data/var/lib/docker/volumes/mynfs/_data/video-$bootnum-%05d-$today.mkv max-size-time=300000000000 > /data/log.txt 2> /data/error-log.txt
 
 #gst-launch-1.0 videotestsrc is-live=1 num-buffers=600 ! video/x-raw,width=3840,height=2160,framerate=60/1 ! nvvidconv ! 'video/x-raw(memory:NVMM), width=(int)3840, height=(int)2160, format=(string)NV12' ! nvv4l2h264enc maxperf-enable=1 bitrate=8000000 ! h264parse ! qtmux ! filesink location=/data/var/lib/docker/volumes/mynfs/_data/filename_h264.mp4 -e
 
